15fb1a7268f053dec75765dbcada613b2740b541
mertolbekirov <mertol2002@gmail.com>
2021-08-17 21:57:59 +0300

    completed SalesControllerTests

--- CSGOMarketplace.Tests/Controllers/Admin/SalesControllerTest.cs
+++ CSGOMarketplace.Tests/Controllers/Admin/SalesControllerTest.cs
@@ -1,12 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
+using MyTested.AspNetCore.Mvc;
+using Xunit;
+using FluentAssertions;
+using CSGOMarketplace.Areas.Admin.Controllers;
+using CSGOMarketplace.Data.Models;
+using static CSGOMarketplace.Areas.Admin.AdminConstants;
+using static CSGOMarketplace.Tests.Data.Sales;
 
 namespace CSGOMarketplace.Tests.Controllers.Admin
 {
     public class SalesControllerTest
     {
+        [Fact]
+        public void UnresolvedSalesShouldReturnViewCorrectly()
+            => MyController<SalesController>
+                .Instance(controller => controller
+                    .WithData(TenUnresolvedSales)
+                    .WithUser(user => user.InRole(AdministratorRoleName)
+                    ))
+                .Calling(x => x.UnresolvedSales())
+                .ShouldReturn()
+                .View();
+
+        [Fact]
+        public void ResolvedSalesShouldReturnViewCorrectly()
+            => MyController<SalesController>
+                .Instance(controller => controller
+                    .WithData(TenResolvedSales)
+                    .WithUser(user => user.InRole(AdministratorRoleName)
+                    ))
+                .Calling(x => x.ResolvedSales())
+                .ShouldReturn()
+                .View();
+
+        [Fact]
+        public void ResolveShouldResolveTheSaleAndSaveToDbCorrectly()
+            => MyController<SalesController>
+                .Instance(controller => controller
+                    .WithData(UnresolvedSale)
+                    .WithUser(user => user.InRole(AdministratorRoleName)
+                    ))
+                .Calling(x => x.Resolve(1))
+                .ShouldHave()
+                .Data(data => data
+                    .WithSet<Sale>(sales => sales
+                        .FirstOrDefault(sale => sale.IsResolved == true).Should().NotBeNull()))
+                .AndAlso()
+                .ShouldReturn()
+                .Redirect();
+
+        [Fact]
+        public void UnResolveShouldResolveTheSaleAndSaveToDbCorrectly()
+            => MyController<SalesController>
+                .Instance(controller => controller
+                    .WithData(ResolvedSale)
+                    .WithUser(user => user.InRole(AdministratorRoleName)
+                    ))
+                .Calling(x => x.Unresolve(1))
+                .ShouldHave()
+                .Data(data => data
+                    .WithSet<Sale>(sales => sales
+                        .FirstOrDefault(sale => sale.IsResolved == false).Should().NotBeNull()))
+                .AndAlso()
+                .ShouldReturn()
+                .Redirect();
     }
 }
---
+++ CSGOMarketplace.Tests/Data/Sales.cs
@@ -0,0 +1,36 @@
+﻿using CSGOMarketplace.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSGOMarketplace.Tests.Data
+{
+    class Sales
+    {
+        public static IEnumerable<Sale> TenUnresolvedSales
+            => Enumerable.Range(0, 10).Select(i => new Sale
+            {
+                IsResolved = false
+            });
+
+        public static IEnumerable<Sale> TenResolvedSales
+            => Enumerable.Range(0, 10).Select(i => new Sale
+            {
+                IsResolved = true
+            });
+
+        public static Sale UnresolvedSale
+            => new Sale
+            {
+                IsResolved = false
+            };
+
+        public static Sale ResolvedSale
+            => new Sale
+            {
+                IsResolved = false
+            };
+    }
+}

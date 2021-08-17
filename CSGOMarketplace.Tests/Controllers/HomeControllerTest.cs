176d6bd738b8dc3b666f61dddf12ffdf79bcb794
mertolbekirov <mertol2002@gmail.com>
2021-08-17 15:19:39 +0300

    Home controller tests

---
+++ CSGOMarketplace.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using CSGOMarketplace.Controllers;
+using CSGOMarketplace.Services.Items.Models;
+using FluentAssertions;
+using MyTested.AspNetCore.Mvc;
+using Xunit;
+
+using static CSGOMarketplace.WebConstants.Cache;
+using static CSGOMarketplace.Tests.Data.Items;
+
+namespace CSGOMarketplace.Tests.Controllers
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void IndexShouldReturnCorrectViewWithModel()
+            => MyController<HomeController>
+                .Instance(controller => controller
+                    .WithData(TenPublicCars))
+                .Calling(c => c.Index())
+                .ShouldHave()
+                .MemoryCache(cache => cache
+                    .ContainingEntry(entry => entry
+                        .WithKey(LatestItemsCacheKey)
+                        .WithAbsoluteExpirationRelativeToNow(TimeSpan.FromMinutes(15))
+                        .WithValueOfType<List<LatestItemServiceModel>>()))
+                .AndAlso()
+                .ShouldReturn()
+                .View(view => view
+                    .WithModelOfType<List<LatestItemServiceModel>>()
+                    .Passing(model => model.Should().HaveCount(3)));
+
+        [Fact]
+        public void ErrorShouldReturnView()
+            => MyController<HomeController>
+                .Instance()
+                .Calling(c => c.Error())
+                .ShouldReturn()
+                .View();
+    }
+}
---
+++ CSGOMarketplace.Tests/Data/Items.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CSGOMarketplace.Data.Models;
+
+namespace CSGOMarketplace.Tests.Data
+{
+    class Items
+    {
+        public static IEnumerable<Item> TenPublicCars
+            => Enumerable.Range(0, 10).Select(i => new Item
+            {
+            });
+    }
+}
